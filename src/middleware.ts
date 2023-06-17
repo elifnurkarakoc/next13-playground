5a64aad6e1996d913bb8e00341b0f59293db282b
elifnurkarakoc <elifnurkarakoc@gmail.com>
2023-06-17 19:52:34 +0300

    initial commit

---
+++ src/middleware.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+
+const allowedOrigins = process.env.NODE_ENV === 'production' ? ['https://www.site.com', 'https://site.com'] : ['http://localhost:3000', 'https://www.google.com']
+
+export function middleware(request: Request) {
+
+    // const regex = new RegExp('/api/*')
+
+    // if (regex.test(request.url)) {
+
+    // }
+
+    const origin = request.headers.get('origin')
+    console.log(origin);
+
+    if (origin && !allowedOrigins.includes(origin)) {
+        return new NextResponse(null, {
+            status: 400,
+            statusText: 'Bad Requests',
+            headers: {
+                'Content-Type': 'text/plain'
+            }
+        })
+    }
+
+    console.log("Middleware!")
+    console.log(request.method);
+    console.log(request.url);
+
+
+
+    return NextResponse.next()
+
+}
+
+export const config = {
+    matcher: "/api/:path*",
+}
